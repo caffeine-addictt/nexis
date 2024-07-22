9da80be9958351ddf40f517d57cd702ce136bfc5
AlexNg <contact@ngjx.org>
2024-07-22 17:48:05 +0800

    feat: Add root homepage
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/routes.go
+++ cmd/routes.go
@@ -1,7 +1,13 @@
 package cmd
 
-import "net/http"
+import (
+	"net/http"
+
+	rootHandlers "github.com/caffeine-addictt/nexis/cmd/routes"
+)
 
 // Here is where we will register every single endpoint
 func RegisterRoutes(router *http.ServeMux) {
+	// Register /
+	router.HandleFunc("/", rootHandlers.GetRoot)
 }
